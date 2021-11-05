712156e3fad6daa8e0493eb7e8a400eb393f3113
josymarss <sylvajosymar@gmail.com>
2021-11-05 03:21:17 +0100

    Make some queries

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -7,21 +7,39 @@ const App = require('../database/models/app');
 
 // ** section user ** //
 router.post('/user/create', async (req,res) =>{
-      //criar usuario no banco de dados
+      //criar usuario no banco de dados ** 
+      const query = {"phone":req.body.phone};
       const user = req.body;
-      const userCreated = await User.create(user);
-      res.send( userCreated);
+      let alredyExist = await User.findOne(query);
+      console.log(alredyExist);
+      if(!alredyExist){
+            await User.create(user);
+            return res.send(true);
+      }else{
+            return res.send(false);
+      }
 });
 router.get('/users',async (req,res) =>{
-      //retorna todos, especifico para procurar usuarios
+      //retorna todos, especifico para procurar usuarios ** 
       const users = await User.find();
       res.send(users);
 });
 router.get('/user/:id', async(req,res) =>{
-      //retorna usuario especifico
+      //retorna usuario especifico ** 
       const specificUser = await User.find({_id:req.params.id},{});
       res.send(specificUser);
 });
+router.put('/user/follow/:id', async (req,res)=>{
+      //Seguir um usuario **
+      //Meu id vem do localstorage 
+      const myId = '';
+      const userResult = await User.findOne({_id:myId},{name:1,_id:1});//buscar meu nome
+      const user = await User.updateOne({_id:req.params.id},
+            { $push: { followers: userResult } }
+      );//ir na pessoa que quero seguir e adicionar meu nome
+
+      res.send(user);
+})
 // ** setion app ** //
 router.post('/app/create', async (req,res)=> {
       //Inserir apps com o id do usuario(relacao 1 para m)
@@ -43,22 +61,17 @@ router.get('/app/user/:id', async (req,res)=>{
 });
 // ** setion negotiations ** //
 
-router.put('/api/negociation/:id', async (req,res) =>{
+router.post('/negociation/:id', async (req,res) =>{
       //Ir no app especifico com id e fazer a negociacao
-      const [ nameusario,description,idusernegotiatior ] = req.body;
+      //const { nameusario,description,idusernegotiatior } = req.body;
+      //console.log(req.body);
       const negotiationResult = await App.updateOne(
             {_id:req.params.id},
-            {
-                  $set:{
-                        nameusario,
-                        description,
-                        idusernegotiatior
-                  }
-            }
+            {$push: { negociation:req.body } }      
       );
       res.send(negotiationResult);
 });
 
 
 
-module.exports = router
+module.exports = router;
